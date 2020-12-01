d96b517ac584c733bc58d8314c0237670ccd5721
ericleto1978 <75315917+ericleto1978@users.noreply.github.com>
2020-12-01 08:30:10 -0500

    Add files via upload

---
+++ serviceA.php
@@ -0,0 +1,11 @@
+<?php
+    echo 'Jour:        '. date('Y-m-d') ."\n";
+    echo "Heure UTC:   " . date("h:i:sa") ."\n";
+    echo " (répondu par ".$_SERVER['SERVER_ADDR'].")\n";
+?>
+
+       
+
+
+
+    
